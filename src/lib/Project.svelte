7f2bf5e1ccbcb61da3cc092a4628f8d781a9d1de
StrangeAndStranger <71987096+StrangeAndStranger@users.noreply.github.com>
2024-09-19 12:25:45 -0400

    Added project article each block

---
+++ src/lib/Project.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let data = {};
+</script>
+<article>
+    <h2>{data.title}</h2>
+    <img src={data.image} alt="" style="height: 10%; width: 10%;">
+    <p>{data.description}</p>
+</article>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,8 +1,11 @@
+<script>
+    import projects from '$lib/projects.json';
+</script>
 <svelte:head>
 	<title>Prior projects</title>
 </svelte:head>
 <body>
-    <h1>Projects Page</h1>
+    <h1>{ projects.length } Projects</h1>
 
     <!-- <nav>
         <ol>
@@ -13,5 +16,13 @@
             <li><a href="https://github.com/StrangeAndStranger" target="_blank">GitHub Profile</a></li>
         </ol>
     </nav> -->
+
+    {#each projects as p}
+        <article>
+            <h2>Project title</h2>
+            <img src="static\images\Cat03.jpg" alt="" style="height: 10%; width: 10%;">
+            <p>Project description</p>
+        </article>
+    {/each}
     
 </body>
