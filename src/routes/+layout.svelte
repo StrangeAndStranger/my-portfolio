829522a499ced0796f70608d6e0d29e5312f68e8
StrangeAndStranger <71987096+StrangeAndStranger@users.noreply.github.com>
2024-10-03 11:28:03 -0400

    Added color scheme change with dropdown

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,14 @@
         {url: "projects/", title: "Projects"},
         {url: "https://github.com/StrangeAndStranger", title: "Github Profile"}
     ];
+
+    
+    let localStorage = globalThis.localStorage ?? {};
+    let colorScheme = localStorage.colorScheme ? localStorage.colorScheme : "light dark";
+    $: localStorage.colorScheme = colorScheme;
+
+    let root = globalThis?.document?.documentElement;
+    $: root?.style.setProperty("color-scheme", colorScheme);
 </script>
 
 <nav>
@@ -15,4 +23,12 @@
     <a href={ p.url } class:current={ "." + $page.route.id === p.url } target={ p.url.startsWith("http") ? "_blank" : null }>{ p.title }</a>
 	{/each}
 </nav>
+<label class="color-scheme" style="position: absolute;">
+    Theme:
+    <select bind:value={colorScheme}>
+        <option>light</option>
+        <option>light dark</option>
+        <option>dark</option>
+    </select>
+</label>`
 <slot />
--- static/global.js
+++ static/global.js
@@ -1,6 +1,14 @@
 console.log("IT’S ALIVE!");
 
 function $$ (selector, context = document) {
+    
+    let select = document.querySelector(selector);
+    select.addEventListener("input", function (event) {
+        console.log("color scheme changed to", event.target.value);
+
+        document.documentElement.style.setProperty("color-scheme", event.target.value);
+    });
+
 	return Array.from(context.querySelectorAll(selector));
 }
 
@@ -9,32 +17,32 @@ function $$ (selector, context = document) {
 // let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
 // currentLink?.classList.add("current");
 
-let pages = [
-    {url: "/", title: "Home"},
-    {url: "contact/", title: "Contacts"},
-    {url: "cv/", title: "CV"},
-    {url: "projects/", title: "Projects"},
-    {url: "https://github.com/StrangeAndStranger", title: "Github Profile"}
-];
+// let pages = [
+//     {url: "/", title: "Home"},
+//     {url: "contact/", title: "Contacts"},
+//     {url: "cv/", title: "CV"},
+//     {url: "projects/", title: "Projects"},
+//     {url: "https://github.com/StrangeAndStranger", title: "Github Profile"}
+// ];
 
-let nav2 = document.createElement("nav");
-document.body.prepend(nav2);
+// let nav2 = document.createElement("nav");
+// document.body.prepend(nav2);
 
-for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
-    // nav2.insertAdjacentHTML("beforeend", `<a href="${ url }">${ title }</a>` );
+// for (let p of pages) {
+//     let url = p.url;
+//     let title = p.title;
+//     // nav2.insertAdjacentHTML("beforeend", `<a href="${ url }">${ title }</a>` );
     
-    let a = document.createElement("a");
-    a.href = url;
-    a.textContent = title;
-    nav2.append(a);
+//     let a = document.createElement("a");
+//     a.href = url;
+//     a.textContent = title;
+//     nav2.append(a);
 
-    const ARE_WE_HOME = document.documentElement.classList.contains("home");
-    url = !ARE_WE_HOME && !url.startsWith("http") ? "../" + url : url;
+//     const ARE_WE_HOME = document.documentElement.classList.contains("home");
+//     url = !ARE_WE_HOME && !url.startsWith("http") ? "../" + url : url;
 
-    if (a.host === location.host && a.pathname === location.pathname) {
-        a.classList.add("current");
-        a.classList.toggle("current", a.host === location.host && a.pathname === location.pathname);
-    }
-}
+//     if (a.host === location.host && a.pathname === location.pathname) {
+//         a.classList.add("current");
+//         a.classList.toggle("current", a.host === location.host && a.pathname === location.pathname);
+//     }
+// }
